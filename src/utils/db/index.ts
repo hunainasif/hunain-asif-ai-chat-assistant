dbb241cd53109d27d327d6c19d51e46f9c92101e
hunainasif <hunainasif960@gmail.com>
2025-10-07 15:17:57 +0500

    fix: prisma singleton and correct database url

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["error"],
-  });
+export const prisma = global.__prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.__prisma = prisma;
+}
